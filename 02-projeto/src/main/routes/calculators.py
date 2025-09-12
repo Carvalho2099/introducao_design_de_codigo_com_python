ad8fbcc1ac2724bcdf7913f88055453653bff07d
Felipe Carvalho <carvalhotkd@gmail.com>
2025-09-12 14:02:28 -0400

    rota da segunda calculadora

--- 02-projeto/src/main/routes/calculators.py
+++ 02-projeto/src/main/routes/calculators.py
@@ -1,11 +1,20 @@
 from flask import Blueprint, jsonify, request
 from calculators.calculator_1 import Calculator1
-
+from calculators.calculator_2 import Calculator2
+from drivers.numpy_handler import NumpyHandler
 calc_route_bp = Blueprint("calc_route", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     calc = Calculator1()
+    response = calc.calculate(request)
+
+    return jsonify(response), 200
+
+@calc_route_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    driver = NumpyHandler()
+    calc = Calculator2(driver)
     response = calc.calculate(request)
 
     return jsonify(response), 200
